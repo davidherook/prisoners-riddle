848d118dec349c07517ea8c68bce0542a690913b
davidherook <david.herook@flywire.com>
2022-12-23 15:52:44 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Prisoner Riddle
+Simulate the [prisoner's riddle](https://www.youtube.com/watch?v=iSNsgj1OCLA).
+
+## Run the Simulation
+
+Open terminal and clone the repository. Navigate to that directory, create a virtual environment and install requirements:
+```
+$ git clone https://github.com/davidherook/prisoners-riddle.git
+$ cd prisoners-riddle
+$ python3 -m venv venv
+$ source venv/bin/activate
+$ pip install -r requirements.txt
+```
+
+Run the simulation defining the number of `--rounds` and `--prisoners`. You will see the results printed to the screen. Results for each round will be saved to a folder named `results`.
+
+```
+$ python simulation.py --rounds 1000 --prisoners 100
+
+**************************************************
+
+Simulating 1000 rounds with 100 prisoners each round...
+Saved results to results/results_1671828090.csv
+
+Results Summary:
+
+Prisoners per Round 100
+Rounds Simulated 1000
+Rounds Successful 314
+% Success 0.314
+
+**************************************************
+```
---
+++ simulation.py
@@ -0,0 +1,94 @@
+import os
+import time
+import random
+import argparse
+import pandas as pd
+
+RUN_TIME = int(time.time())
+
+def create_board(prisoners):
+	board = {}
+	boxes = list(range(1, prisoners + 1))
+	papers = list(range(1, prisoners + 1))
+	random.shuffle(papers)
+	for b in boxes:
+		board[b] = papers[b - 1]
+	return board
+
+def is_correct(selection, prisoner_id):
+	if selection == prisoner_id:
+		return True
+	return False
+
+def single_prisoner_attempt(board, prisoner_id):
+	success = False
+	attempts = int(len(board) / 2)
+	box_selected = prisoner_id
+	for a in range(attempts):
+		selection = board[box_selected]
+		#print(f"Prisoner: {prisoner_id}; Attempt: {a}; Box Selection: {box_selected}; Result: {selection}")
+		if is_correct(selection, prisoner_id):
+			success = True
+			break
+		box_selected = selection
+	return success
+
+def simulate(n_rounds, n_prisoners):
+	prisoners = list(range(1, n_prisoners + 1))
+	round_num = []
+	prisoners_successful = []
+	for r in range(n_rounds):
+		board = create_board(n_prisoners)
+		single_round_result = []
+		for prisoner in prisoners:
+			result = single_prisoner_attempt(board, prisoner)
+			single_round_result.append(result)
+		num_prisoners_successful = sum(single_round_result)
+		round_num.append(r)
+		prisoners_successful.append(num_prisoners_successful)
+	return pd.DataFrame({
+		'round': round_num, 
+		'prisoners_successful': prisoners_successful})
+
+def save_results(df_results):
+	dir_out = 'results'
+	path = os.path.join(dir_out, f"results_{RUN_TIME}.csv")
+	if not os.path.isdir(dir_out):
+		os.mkdir(dir_out)
+	df_results.to_csv(path, index = False)
+	print(f"Saved results to {path}")
+
+def print_summary(df_results, rounds, prisoners):
+	filter_success = df_results['prisoners_successful'] == prisoners
+	successful_rounds = df_results[filter_success].shape[0]
+	results = {
+		'Prisoners per Round': prisoners,
+		'Rounds Simulated': rounds,
+		'Rounds Successful': successful_rounds,
+		'% Success': successful_rounds / rounds
+	}
+	print('\nResults Summary:\n')
+	for k,v in results.items():
+		print(k, v)
+
+
+if __name__ == '__main__':
+	header = '\n' + '*' * 50 + '\n'
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-r', '--rounds', type = int, help = 'The number of rounds to simulate')
+	parser.add_argument('-p', '--prisoners', type = int, help = 'The number of prisoners')
+	args = vars(parser.parse_args())
+	rounds = args['rounds']
+	prisoners = args['prisoners']
+
+	print(header)
+	print(f"Simulating {rounds} rounds with {prisoners} prisoners each round...")
+	df_results = simulate(rounds, prisoners)
+	save_results(df_results)
+	print_summary(df_results, rounds, prisoners)
+	print(header)
+
+
+
+
+
